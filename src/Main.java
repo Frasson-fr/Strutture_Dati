ed384a14c69801b2b043b6b020c1b5746e6f7dc4
12999416 <12999416@itisrossi.vi.it>
2026-09-22 09:26:09 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,89 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import static java.lang.IO.println;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+void main() {
+//
+//    int nElementi = 100_000;
+//
+//    List<Integer> arrayList = new ArrayList<>();
+//    List<Integer> linkedList = new LinkedList<>();
+//
+//    long start = System.nanoTime();
+//
+//    for (int i = 0; i < nElementi; i++) {
+//        arrayList.add(i);
+//    }
+//
+//    long end = System.nanoTime();
+//    println("Tempo Array List - Inserimento in coda: " + (end-start)/1_000_000.0 + " ns");
+//
+//    start = System.nanoTime();
+//
+//    for (int i = 0; i < nElementi; i++) {
+//        linkedList.add(i);
+//    }
+//
+//    end = System.nanoTime();
+//    println("Tempo Linked List - Inserimento in coda: " + (end-start)/1_000_000.0 + " ns");
+//
+//    ///// Accesso casuale
+//    start = System.nanoTime();
+//
+//    for (int i = 0; i < 1_000; i++) {
+//        arrayList.get(nElementi/2);
+//    }
+//
+//    end = System.nanoTime();
+//    println("Tempo Array List - Accessi al centro: " + (end-start)/1_000_000.0 + " ns");
+//
+//    start = System.nanoTime();
+//
+//    for (int i = 0; i < 1_000; i++) {
+//        linkedList.get(nElementi/2);
+//    }
+//
+//    end = System.nanoTime();
+//    println("Tempo Linked List - Accessi al centro: " + (end-start)/1_000_000.0 + " ns");
+//
+//
+//    /// Hash
+//    String chiave = "Utente_Barin_2008";
+//    int numeroBucket = 16;
+//
+//    int hashCode = chiave.hashCode();
+//    int indiceBucket = Math.abs(hashCode) % numeroBucket;
+//    println("chiave " + chiave + " hashCode " +  hashCode + " indiceBucket " + indiceBucket);
+
+    int nElementi = 1_000_000;
+    int target = nElementi - 1;
+
+    List<Integer> arrayList = new ArrayList<>();
+    Set<Integer> treeSet = new TreeSet<>();
+    Set<Integer> hashSet = new HashSet<>();
+
+    for (int i = 0; i < nElementi; i++) {
+        // arrayList.add(i);
+        treeSet.add(i);
+        // hashSet.add(i);
+    }
+
+////  TODO arrayList
+//    long start = System.nanoTime();
+//    boolean trovatoElemento = arrayList.contains(target);
+//    long end = System.nanoTime();
+//    double tempo = (end - start) / 1_000_000.0;
+//    println("ArrayList O(n) Trovato: " + trovatoElemento + " Tempo: " + tempo + " ns");
+
+//// TODO treeSet
+    long start = System.nanoTime();
+    boolean trovatoElemento = treeSet.contains(target);
+    long end = System.nanoTime();
+    double tempo = (end - start) / 1_000_000.0;
+    println("treeSet O(log n) Trovato: " + trovatoElemento + " Tempo: " + tempo + " ns");
+
+//// TODO hashSet
+    start = System.nanoTime();
+    trovatoElemento = hashSet.contains(target);
+    end = System.nanoTime();
+    tempo = (end - start) / 1_000_000.0;
+    println("hashSet O(1) Trovato: " + trovatoElemento + " Tempo: " + tempo + " ns");
 }
